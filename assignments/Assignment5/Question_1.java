fa11d0b810cc65ce58ecbf60e17f3d6ab3a5323f
fancytseng <55032412+fancytseng@users.noreply.github.com>
2019-10-10 16:09:18 -0700

    Q1

---
+++ assignments/Assignment5/Question_1.java
@@ -0,0 +1,21 @@
+package Assignment5;
+
+public class Question_1 {
+    //Explain the difference between final and finally in Java 8.
+
+    /*
+    Final: It is done. It cannot be changed.
+        If the parents are final method, then the children cannot change the method.
+        If one class is final, then no other classes can extend that class.
+        Final class can't be inherited.
+        If made data final, then it cannot be modified.
+        Final variable value can't be changed.
+        Method with final scope can't be overridden in child class.
+        It is a keyword.
+
+    Finally: It is a piece of code that always perform, even you have an exception.
+        We use finally if we have something that we really need to do.
+        Finally is used to place important code, it will be executed whether exception is handled or not.
+        It is a block.
+     */
+}
